1dcaa1704d9e723ca9661c49a0a835c74cabca72
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-04-28 23:24:22 +0530

    app setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Welcome to React Router!
+# SereneNomad
 
 A modern, production-ready template for building full-stack React applications using React Router.
 
--- app/root.tsx
+++ app/root.tsx
@@ -32,9 +32,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+
       <body>
         {children}
+
         <ScrollRestoration />
+
         <Scripts />
       </body>
     </html>
@@ -64,7 +67,9 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   return (
     <main className="pt-16 p-4 container mx-auto">
       <h1>{message}</h1>
+
       <p>{details}</p>
+
       {stack && (
         <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
